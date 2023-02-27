49ae9e0f227ee9b22cd4ef1bc06a9eec9a152f55
ZZohanDvir <98888100+ZZohanDvir@users.noreply.github.com>
2023-02-27 20:30:53 +0400

    Add files via upload
    lesson 7 homework remake

---
+++ lesson7/car.rb
@@ -0,0 +1,31 @@
+require_relative 'manufacturer.rb'
+require_relative 'validation.rb'
+
+class Car
+  
+  include Manufacturer
+  include Validation
+  attr_reader :number
+  attr_accessor :hooked
+
+  def initialize(number)
+    @number = number
+    validate_car(number)
+  end
+
+  def hook
+    @hooked = true
+  end
+
+  def unhook
+    @hooked = false
+  end
+
+  def valid?
+    validate_car(self.number)
+    true
+  rescue
+    false
+  end
+
+end
